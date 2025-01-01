70b8ec8d8cfe3b472330a2e4095dcb0038c182e0
Alexandro Kalinga <149481338+lexan-hacker@users.noreply.github.com>
2025-01-01 20:24:52 +0300

    While Loop

---
+++ grading using while loop.cpp
@@ -0,0 +1,58 @@
+// Grading system using while loop
+
+#include <iostream>
+
+using namespace std ;
+
+int main () {
+	
+	int i = 0 ;
+	
+	cout << " Welcome to Lexan Grading System using Loops \n " ;
+	
+	
+	while ( i < 20 ) {
+		
+		int num ;
+	cout << " Enter your Marks \n " ;
+	cin >> num ;
+	
+	if ( num >= 75 and num <=100 ) {
+		
+		cout << " Your Grade is : A \n " ;
+	}
+	
+	else if ( num >= 65 and num < 75) {
+		
+		cout << " Your Grade is : B \n " ;
+	}
+	
+	else if ( num >= 45 and num < 65 ) {
+		
+		cout << " Your Grade is : C \n " ;
+		
+	}
+	
+	else if ( num >=30 and num < 45 ) {
+		
+		cout << " Your Grade is : D \n " ;
+	}
+	
+	else if ( num >= 0 and num < 30 ) {
+		
+		cout << " Your Grade is : F \n " ;
+		
+	}
+	
+	
+	else {
+		
+		cout << " Please input correct marks \n " ;
+	}
+		
+		
+		i++ ;
+	}
+	
+	return 0 ;
+}
