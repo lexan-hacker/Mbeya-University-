e5386588eaf895962bff8dce135e012a2b609a5d
Alexandro Kalinga <149481338+lexan-hacker@users.noreply.github.com>
2025-01-01 20:54:35 +0300

    Checking if a year is a Leap Year

---
+++ Checking Leap Year.py
@@ -0,0 +1,11 @@
+# checking leap year 
+
+year =  float ( input ( " Enter your year of Birth : ") )
+
+if year % 4 == 0 : 
+    print ( " This is a Leap year ")
+
+else :
+    print ( " This is NOT a Leap year ")
+
+print ( " This is the end of the program ")
